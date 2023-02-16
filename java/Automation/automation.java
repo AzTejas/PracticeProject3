62d21f31c878801b643ad7354a8abbe28bb49360
AzTejas <tejaspatel191@gmail.com>
2023-02-16 21:53:03 +0000

    first commit

---
+++ java/Automation/automation.java
@@ -0,0 +1,12 @@
+package Automation;
+
+public class automation {
+    public static void main(String[] args) {
+        System.out.println("Automation Topics");
+        System.out.println("Importance of Automation");
+        System.out.println("What is Java");
+        System.out.println("Features of Java");
+        System.out.println("IDE");
+        System.out.println("Packages");
+    }
+}
---
+++ java/Manual/GroupA.java
@@ -0,0 +1,11 @@
+package Manual;
+//renamed alpha from Alpha
+public class GroupA {
+    public static void main(String[] args) {
+        System.out.println("Today's Class is Fun");
+        System.out.println("Manual Topics");
+        System.out.println("Test Cases");
+        System.out.println("Test Plan");
+        System.out.println("RTM");
+    }
+}
